dae655904f2e4231a4e95c028045cf4282364e42
ynafey <ynafey@gmail.com>
2017-01-21 16:32:01 +0530

    IsDead added

--- Assets/scripts/CameraBehaviour.cs
+++ Assets/scripts/CameraBehaviour.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour {
-
     public GameObject tick;
-
+    private float maxHeight;
 	
 	void Update () {
-        this.transform.position = new Vector3(this.transform.position.x, this.tick.transform.position.y, this.transform.position.z);
+        if (this.tick.transform.position.y > maxHeight) {
+            this.transform.position = new Vector3(this.transform.position.x, this.tick.transform.position.y, this.transform.position.z);
+            this.maxHeight = this.tick.transform.position.y;
+        }
 	}
 }
---
+++ Assets/scripts/TickBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TickBehaviour : MonoBehaviour {
+    private Plane[] planes;
+    
+
+    public bool isDead = false;
+    // when tick dies set is dead flag
+    
+    void Start() {
+        planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+    }
+
+
+    void Update() {
+        if (!GeometryUtility.TestPlanesAABB(planes, this.GetComponent<Collider2D>().bounds)) {
+            this.isDead = true;
+        }
+    }
+}
