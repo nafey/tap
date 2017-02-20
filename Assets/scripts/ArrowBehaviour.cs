b161db222e73e269e6ba3b45be2464c147170907
ynafey <ynafey@gmail.com>
2017-02-20 23:44:28 +0530

    Arrow and Trajectory

--- Assets/scripts/ArrowBehaviour.cs
+++ Assets/scripts/ArrowBehaviour.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrowBehaviour : MonoBehaviour {
+    private Vector3[] line;
+    private LineRenderer lineRenderer;
+    public Transform tick;
+    public Vector3 start;
+    public float accel;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void Start() {
+        this.lineRenderer = this.GetComponent<LineRenderer>();
+        this.redrawLine();
+    }
+
+    public void redrawLine() {
+        Vector3 startVelocity = start;
+
+        Vector3[] trajectory = Support.computeTrajectory(startVelocity, accel, 
+            tick.position, 1    );
+
+        this.renderLine(trajectory);
+    }
+
+    public void renderLine(Vector3[] newLine) {
+        line = newLine;
+        this.lineRenderer.numPositions = newLine.Length;
+        this.lineRenderer.SetPositions(line);
+    }   
 }
--- Assets/scripts/Support.cs
+++ Assets/scripts/Support.cs
@@ -14,11 +14,47 @@ public class Support {
         return instance;
     }
 
-    public Vector3 computeVelocity(Vector3 normal, Vector3 velocity) {
+    public static Vector3 computeVelocity(Vector3 normal, Vector3 velocity) {
         Vector3 tangent = new Vector3(-normal.y, normal.x, 0);
         float a = Vector3.Dot(velocity, normal);
         float b = Vector3.Dot(velocity, tangent);
 
         return a * normal + b * tangent;
     }
+
+    public static float computeHeight(float v0, float a, float time) {
+        return v0 * time - a * time * time / 2f;
+    }
+
+    public static Vector3[] computeTrajectory(Vector3 startVelocity, float acceleration, 
+        Vector3 origin, float bound) {
+        int size = 5;
+        Vector3[] ret = new Vector3[size];
+
+        float vy = startVelocity.y;
+        float vx = startVelocity.x;
+
+        float timeToFall = vy / acceleration;
+        float timeToBoundary = (bound - origin.x) / vx;
+
+        
+        float timeMax = float.MaxValue;
+
+        if (timeToFall > timeToBoundary) {
+            timeMax = timeToBoundary;
+        } else {
+            timeMax = timeToFall;
+        }
+        
+        for (int i = 0; i < size; i++) {
+            float t = timeMax * (((float) i) / ((float) (size - 1f)));
+            float x = vx * t;
+            float y = computeHeight(vy, acceleration, t);
+
+            ret[i] = new Vector3(x, y) + origin;
+        }
+        
+        return ret;
+    }
 }
+
